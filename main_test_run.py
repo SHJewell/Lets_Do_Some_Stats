a41c5c47f54f87cdb507dbffcc7ba58e35027db6
SHJewell <scott.hjewell@gmail.com>
2022-06-26 22:26:10 -0700

    Structure works. Now to build better regressions.

--- main_test_run.py
+++ main_test_run.py
@@ -1,8 +1,12 @@
 '''
 Main functions for calling stat methods, comparing with pre-existing packages, loading data etc
+
+housing data from:
+
 '''
 
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 import numpy as np
 import scipy
 from matplotlib import pyplot as plt
@@ -13,9 +17,23 @@ import plotters
 
 import regression_tests as regtest
 
-house_data_path = f'E:\Documents\Datasets\Kaggle\House Prices\\train.csv'
+# house_data_path = f'E:\Documents\Datasets\Kaggle\House Prices\\train.csv'
+house_data_path = 'C:\\Users\\Scrooge\\Documents\\Data\\Kaggle\\Housing\\train.csv'
+
 
 if __name__ == '__main__':
 
+    data = pd.read_csv(house_data_path)
 
-    regtest.linear_reg_test(house_data_path)
+    for col in data.columns:
+
+        # if col == 'SalePrice':
+        #
+        #     continue
+
+        if is_numeric_dtype(data[col]):
+
+            selected_data = data[['SalePrice', col]]
+            selected_data.dropna(axis='index', inplace=True)
+
+            regtest.linear_reg_test(selected_data, col, 'SalePrice')
--- regression_tests.py
+++ regression_tests.py
@@ -6,43 +6,39 @@ import regression
 import error_calcs
 
 
-def linear_reg_test(data_path):
+def linear_reg_test(data, x_name, y_name):
 
-    data = pd.read_csv(data_path)
-
-    yearbuilt = data['YearBuilt'].to_numpy()
-    price = data['SalePrice'].to_numpy()
+    yearbuilt = data[x_name].to_numpy()
+    price = data[y_name].to_numpy()
 
     #plts.scatter_2d(price, yearbuilt, y_label='Year Built', x_label='Price')
 
     my_alpha, my_intercept = regression.my_linear_reg(yearbuilt, price)
-    sk_alpha, sk_intercept, sk2_model_price = regression.skl_linear_reg(yearbuilt, price, pred=True)
+    sk_alpha, sk_intercept, sk_model_price = regression.skl_linear_reg(yearbuilt, price, pred=True)
 
     my_model_price = yearbuilt * my_alpha + my_intercept
-    sk1_model_price = yearbuilt * sk_alpha[0] + sk_intercept
 
     my_residual = np.absolute(price - my_model_price)
-    sk1_residual = np.absolute(price - sk1_model_price)
-    sk2_residual = np.absolute(price - sk2_model_price)
+    sk_residual = np.absolute(price - sk_model_price)
 
-    model_resid_1 = np.absolute(my_model_price - sk1_model_price)
-    model_resid_2 = np.absolute(my_model_price - sk2_model_price)
+    model_resid = np.absolute(my_model_price - sk_model_price)
 
-    print(error_calcs.rmse(my_model_price, price), error_calcs.rmse(sk1_model_price, price), error_calcs.rmse(sk2_model_price, price))
+    print(error_calcs.rmse(my_model_price, price), error_calcs.rmse(sk_model_price, price))
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.scatter(yearbuilt, price, marker='.', color='blue', label='Price')
-    ax1.scatter(yearbuilt, my_model_price, marker='+', color='green', label='My Price')
-    ax1.scatter(yearbuilt, sk1_model_price, marker='*', color='red', label='SKL1 Price')
-    ax1.scatter(yearbuilt, sk2_model_price, marker='*', color='firebrick', label='SKL2 Price')
-    ax1.xtitle='Price'
-    ax1.legend()
-    ax2.scatter(yearbuilt, my_residual, marker='+', color='green', label='My Resid')
-    ax2.scatter(yearbuilt, sk1_residual, marker='*', color='red', label='SKL1 Resid')
-    ax2.scatter(yearbuilt, sk2_residual, marker='*', color='firebrick', label='SKL2 Resid')
-    ax2.legend()
-    ax3.scatter(yearbuilt, model_resid_1, marker='.', label='Model Resid 1')
-    ax3.scatter(yearbuilt, model_resid_2, marker='.', label='Model Resid 2')
-    ax3.legend()
+    plt.subplot(3, 1, 1)
+    plt.scatter(yearbuilt, price, marker='.', color='blue', label='Price')
+    plt.plot(yearbuilt, my_model_price, color='green', label='My Price')
+    plt.plot(yearbuilt, sk_model_price, color='red', label='SKL1 Price')
+    plt.title(f'{x_name} vs {y_name}')
+    plt.legend()
+
+    plt.subplot(3, 1, 2)
+    plt.scatter(yearbuilt, my_residual, marker='+', color='green', label='My Resid')
+    plt.scatter(yearbuilt, sk_residual, marker='*', color='red', label='SKL1 Resid')
+    plt.legend()
+
+    plt.subplot(3, 1, 3)
+    plt.scatter(yearbuilt, model_resid, marker='.', label='Model Resid')
+    plt.legend()
 
     plt.show()
