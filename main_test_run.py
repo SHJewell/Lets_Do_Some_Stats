3f6cd7762b32e1d8a17aca30d8e53c5743228c18
SHJewell <scott.hjewell@gmail.com>
2022-07-28 19:06:39 -0700

    Added local data. Upated loop for testing VIF

--- main_test_run.py
+++ main_test_run.py
@@ -2,6 +2,7 @@
 Main functions for calling stat methods, comparing with pre-existing packages, loading data etc
 '''
 
+# their imports
 import pandas as pd
 import numpy as np
 import scipy
@@ -11,31 +12,31 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn import linear_model
 import statsmodels.api as sm
 
+# my imports
 import regression
 import error_calcs
 import plotters
 import other_analysis as other
 import regression_tests as regtest
 
-house_data_path = f'E:\\Documents\\Datasets\\Kaggle\\House Prices\\train.csv'
-small_bp_set = f'E:\\Documents\\Datasets\\Toy Sets\\Bloodpress\\bloodpress.csv'
-toy_stocks_set =f'E:\Documents\Datasets\Toy Sets\Stockmarket\stockmarket.csv'
+# house_data_path = f'E:\\Documents\\Datasets\\Kaggle\\House Prices\\train.csv'
+# small_bp_set = f'E:\\Documents\\Datasets\\Toy Sets\\Bloodpress\\bloodpress.csv'
+# toy_stocks_set =f'E:\\Documents\\Datasets\\Toy Sets\\Stockmarket\\stockmarket.csv'
+small_bp_set = f'Data/blood_pressure.csv'
 
 if __name__ == '__main__':
 
     bp = pd.read_csv(small_bp_set)
 
     xs = ['Pt', 'Age', 'Weight', 'BSA', 'Dur', 'Pulse', 'Stress']
-
     bpy = bp['BP']
-
     cor_mat = np.array()
 
     for n, col in enumerate(bp.columns):
 
-        for m in range(n,len(col)):
+        for m in range(n, len(col)):
 
-
+            cor_mat[n, m] = error_calcs.rsquared(bpy, col)
 
     bp_dy, bp_dX = dmatrices('BP ~ Age+Weight+BSA+Dur+Pulse+Stress', data=bp, return_type='dataframe')
 
